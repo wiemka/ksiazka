d0f5b5dd1af5824f8f21922512a3c06290320ba3
Magda Wielgosz <wielgosz.m@vp.pl>
2017-08-27 16:30:19 +0200

    szablon tekstu

--- js/ksiazka.js
+++ js/ksiazka.js
@@ -1,1 +1,19 @@
 "use strict";
+
+class Ksiazka {
+    constructor(tytul, autor, przeczytana) {
+        this.tytul = tytul;
+        this.autor = autor;
+        this.przeczytana = przeczytana;
+    }
+    opiszKsiazke() {
+        var text = "Książka ma tytuł" + this.tytul + ", autorem jest " + this.autor + " i została ";
+
+        if (this.przeczytana) {
+            text += "przeczytana.";
+        } else {
+            text += "nieprzeczytana";
+        }
+        console.log(text);
+    }
+}
